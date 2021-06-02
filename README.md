15ddec9ad7fd61128e391701e5858280877fa4d0
Fatimah2014 <80806004+Fatimah2014@users.noreply.github.com>
2021-06-01 20:33:47 -0400

    added style to readme

--- README.md
+++ README.md
@@ -1,39 +1,56 @@
-# About0-Me-with-Node.js
-## Description
->**This is a command line tool thatgenerates an HTML [portfolio page form user input.**
-## Table of Content
+# README With Node.js  
+### 06/01/2021
+------
+# Description
+
+- To make life easier after creating a application this app generates the user input and creates a professional README
+- This app was created to provide a professional formated README.md file for the user
+- During the completion of this application an error appeared on the terminal which provided detailed infromation on how to fix the issue. (log was undefined)
+- Overall I learned how to create a README file using Node.js. In addition during the poccess I learned how to properly google an error if I am not sure how to debug the code. 
+
+------
+
+# Table of Content
 - Installation
 
 - Usage
 
+- github
+
+- Credits
+
 - License 
 
-- Contributions: 
-
-- Table of Contents
+- Contributions 
 
 - Test
 
 - Questions
-
-## Installation
+----------
+# Installation
 To install necessary dependencies, run the following command:
 
->**npm i **
+>**npm i**
+--------
+# Usage
+>**Enjoy this application by entering your response in the command-line.**
+---------
+# Credits
+>**Github, Inquirer package, .gitignore**
+------
+# License
 
-## Usage
->**Just pull repo run it and enjoy**
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) 
 
-## License
->**Mit** 
 
-## Contributing
->**Fork the repo and open a pr for any changes** 
+------
+# Contributing
+>Fork the repo and clone locally  
 
-## Test
-To run a test run the following command:
+# Test
+#### To run a test run the following command:
 
 >**npm test**
 
-## questions
- If you have any questions about the Repo, open an issue or contact me directly at <!--Fatimah Grant-->. you can find more of my work at <!--Fatimah2014-->.
+## Questions
+ If you have any questions about the Repo, open an issue or contact me directly at <Fatimahg1102@gmail.com>. you can find more of my work at [GitHub](https://github.com/Fatimah2014).
--- index.js
+++ index.js
@@ -1,38 +1,50 @@
 
 const inquirer = require('inquirer')
 const fs = require('fs')
-const generateReadme = (github, email, title, description, install, usage, license, test, contributions) =>
+const generateReadme = (github, email, title, date, quest1, quest2, quest3, quest4, install, usage,credits, license, test, contributions) =>
 `# ${title}
+### ${date}
+
 ## Description
->**${description}**
+
+>- ${quest1}
+>- ${quest2}
+>- ${quest3}
+>- ${quest4}
+
 ## Table of Content
 - Installation
 
 - Usage
 
+- github
+
+- Credits
+
 - License 
 
-- Contributions: 
-
-- Table of Contents
+- Contributions 
 
 - Test
 
 - Questions
 
 ## Installation
-To install necessary dependencies, run the following command:
+To install necessary dependencies, run the following command
 
 >**${install}**
 
 ## Usage
 >**${usage}**
 
+## Credits
+>**${credits}**
+
 ## License
->**${license}** 
+${license} 
 
 ## Contributing
->**${contributions}** 
+>${contributions} 
 
 ## Test
 To run a test run the following command:
@@ -40,7 +52,7 @@ To run a test run the following command:
 >**${test}**
 
 ## questions
- If you have any questions about the Repo, open an issue or contact me directly at <!--${email}-->. you can find more of my work at <!--${github}-->.` 
+ If you have any questions about the Repo, open an issue or contact me directly at <${email}>. you can find more of my work at [GitHub](https://github.com/${github}).` 
 
 
 
@@ -58,17 +70,41 @@ inquirer.prompt([
     message: 'What is your Project name?'},
 
     {type:'input', 
-    name: 'description', 
-    message: 'Please write a short description of your project'},
+    name: 'date', 
+    message: 'What your project completion date?'},
+
+    {type:'input', 
+    name: 'quest1', 
+    message: 'What is your was your motivation?'},
+
+    {type:'input', 
+    name: 'quest2', 
+    message: 'Why did you build this project?'},
+    
+    {type:'input', 
+    name: 'quest3', 
+    message: 'What problems did you solve?'},
+    
+    {type:'input', 
+    name: 'quest4', 
+    message: 'What did you learn?'},
+
+
+
+
+    {type: 'input',
+    name: 'credits',
+    message: "Please add Credits:"},
 
     {type:'list', 
     name: 'license', 
     message: 'What kind of license should your project have',
     choices: [
-        "Mit",
-        "APACHE",
-        "artistic",
-        "bsl",]},
+        "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+        "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+        "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)",
+        "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)"],
+    },
 
     {type:'input', 
     name: 'install', 
@@ -87,18 +123,24 @@ inquirer.prompt([
      name: 'contributions',
      message: 'What does the user need to know about contributing to the repo?'}]).then(answers => {
     
-        const { github, email, title, description, install, usage, license, test, contributions} = answers
+       
+        const { github, email, title, date, quest1, quest2, quest3, quest4, install, usage, credits, license, test, contributions} = answers
     console.log('github:',github)
     console.log('email:',email)
     console.log('title:',title)
-    console.log('description:',description)
+    console.log('date:', date)
+    console.log('quest1:', quest1)
+    console.log('quest2:', quest2)
+    console.log('quest3:', quest3)
+    console.log('quest4:', quest4)
     console.log('install:',install)
     console.log('usage:',usage)
+    console.log('credits:',credits)
     console.log('license:',license)
     console.log('test:',test)
     console.log('contributions:', contributions)
 
-const readmeContent = generateReadme(github, email, title, description, install, usage, license, test, contributions)
+const readmeContent = generateReadme(github, email, title, date, quest1, quest2, quest3, quest4, install, usage, credits,license, test, contributions)
 console.log('readmeContent:', readmeContent)
 // fs.writeFile('README.md', readmeContent, )
 fs.writeFile('README.md', readmeContent, (err) => err ? console.log(err) : console.log('successfully created README'))
